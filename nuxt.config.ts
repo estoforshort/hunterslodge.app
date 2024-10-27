1498c0802d411d0e3b4079596b82669a704befd5
Esto <esto@hunterslodge.app>
2024-10-27 21:38:35 +0200

    Enable timeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-10-11",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
   future: { compatibilityVersion: 4 },
   extends: ["@nuxt/ui-pro"],
   modules: [
