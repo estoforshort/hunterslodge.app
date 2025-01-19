c51c5a2105a3d87453be5cbc2beb02e0ef1cef61
Esto <esto@hunterslodge.app>
2025-01-19 12:32:43 +0200

    Add users endpoint

---
+++ server/api/users/[user]/index.get.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export default defineCachedEventHandler(
+  async (event) => {
+    const paramsSchema = z.object({
+      user: z.string().min(1).max(36),
+    });
+
+    const params = await getValidatedRouterParams(event, paramsSchema.parse);
+
+    const data = await prisma.user.findUnique({
+      select: {
+        id: true,
+        username: true,
+        displayName: true,
+        isAdmin: true,
+        isFounder: true,
+        createdAt: true,
+        profile: {
+          select: {
+            id: true,
+          },
+        },
+      },
+      where: { id: params.user },
+    });
+
+    return { data };
+  },
+  { maxAge: 60 * 60 },
+);
